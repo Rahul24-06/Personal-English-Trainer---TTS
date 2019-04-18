adfd8b3649c782289dea0554d83fc05233fbefbd
the lonely programmer <rahulkhanna24.06@gmail.com>
2019-04-19 02:25:51 +0530

    Update action-word.py

--- action-word.py
+++ action-word.py
@@ -1,41 +1,35 @@
+
+    
+#    forecast_locality_slot = intent_message.slots.forecast_locality.first()
+
+    
+	
+    
+		
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import random
 from hermes_python.hermes import Hermes
 
 MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
-
 def intent_received(hermes, intent_message):
-    sentence = 'You asked the meaning for  '
-
-    if intent_message.intent.intent_name == '&PM84NKnAGk60OqRQDgeDerZPBkJDpx1m9abBvd5X:FindMeaning':
-        print('FindMeaning')
-#        sentence += 'the weather '
-#    elif intent_message.intent.intent_name == 'searchWeatherForecastTemperature':
-#        print('searchWeatherForecastTemperature')
-#        sentence += 'the temperature '
-    else:
-        return
-
-    givenword_slot = intent_message.slots.givenword.first()
-#    forecast_locality_slot = intent_message.slots.forecast_locality.first()
-
-    if givenword_slot is not None:
-        sentence += givenword_slot.value
+        sentence = 'You asked the meaning for  '
+        if intent_message.intent.intent_name == '&PM84NKnAGk60OqRQDgeDerZPBkJDpx1m9abBvd5X:FindMeaning':
+        	print('FindMeaning')
+        else:
+                return
+	givenword_slot = intent_message.slots.givenword.first()
+        if givenword_slot is not None:
+        	sentence += givenword_slot.value
+		sentence += say + " Rahul "
+	if givenword_slot == 'Recuperate':
+		sentence += 'The meaning of Recuperate is to recover from illness or exertion'
+        hermes.publish_end_session(intent_message.session_id, sentence)
 	
-    if givenword_slot == 'Recuperate':
-	sentence += 'The meaning of Recuperate is to recover from illness or exertion'
-		
-    # if forecast_country_slot is not None:
-        # sentence += 'in ' + forecast_country_slot.value
-    # if forecast_start_datetime_slot is not None and len(forecast_start_datetime_slot) > 0:
-        # sentence += ' ' + forecast_start_datetime_slot[0].raw_value
-
-    hermes.publish_end_session(intent_message.session_id, sentence)
-
-
 with Hermes(MQTT_ADDR) as h:
-    h.subscribe_intents(intent_received).start()
+        h.subscribe_intents(intent_received).start()
